2972d8ec309bfe3840446301186026d9adf9dc11
Jose <josetimog@gmail.com>
2022-01-29 22:52:52 -0600

    Added single player mode

--- app.js
+++ app.js
@@ -3,7 +3,8 @@
 const restartButton = document.getElementById('restart');
 const cells = document.querySelectorAll('.cell');
 const textBox = document.getElementById('text-box');
-let canvas = document.getElementById('myCanvas');
+const canvas = document.getElementById('myCanvas');
+
 
 let turnCount = 1;
 
@@ -14,14 +15,23 @@ const gameboard = {
 }
 
 function renderGameboard() {
-    for (let i = 0; i < 9; i++) {
-        const gridCell = document.getElementById(`${i+1}`);
-        gridCell.textContent = gameboard.board[i];
-    }    
+
+    if (gameMode == 'singlePlayerMode'){
+        for (let i = 0; i < 9; i++) {
+            const gridCell = document.getElementById(`${i+1}-ai`);
+            gridCell.textContent = gameboard.board[i];
+        }   
+    }else if (gameMode == 'multiPlayerMode'){
+        for (let i = 0; i < 9; i++) {
+            const gridCell = document.getElementById(`${i+1}`);
+            gridCell.textContent = gameboard.board[i];
+        }   
+    }
+
+ 
 }
 
 //--------------------------------PLAYERS---------------------------------------------
-
 
 function createPlayer(name) {
     return {
@@ -29,21 +39,11 @@ function createPlayer(name) {
     }
 }
 
-let firstPlayerPrompt = 'First Player';
-let secondPlayerPrompt = 'Second Player';
-// let firstPlayerPrompt = prompt("First player\'s name:");
- let firstPlayer = createPlayer(firstPlayerPrompt);
-// let secondPlayerPrompt = prompt("Second player\'s name:");
- let secondPlayer = createPlayer(secondPlayerPrompt);
-
-
-
 // ----------------------------- CLICKING/HOVERING ON A CELL ----------------------------------
 
 cells.forEach((cell) => {
 
     cell.addEventListener('mouseenter', function cellEnter() {
-
         if(cell.textContent === ''){
             if ((turnCount % 2) == 1) {
                 cell.style.color = 'lightgrey';
@@ -53,35 +53,36 @@ cells.forEach((cell) => {
                 cell.textContent = 'O';
             }
         }
-
     })
 
     cell.addEventListener('mouseleave', function cellLeave() {
-
         if(cell.style.color === 'lightgrey') {
             cell.textContent = '';
             cell.style.color = 'black';
         }
-
     })
 
-
-
     cell.addEventListener('click', function handleClick(){
+
+        let firstPlayerInput = document.getElementById('firstPlayerName').value;
+        let secondPlayerInput = document.getElementById('secondPlayerName').value;
+        let firstPlayer = createPlayer(firstPlayerInput);
+        let secondPlayer = createPlayer(secondPlayerInput);
+
         if ((turnCount % 2) == 1) {
             cell.style.color = 'black';
             gameboard.board[cell.id-1] = 'X';
             renderGameboard();
             cell.style.pointerEvents = 'none';          
             turnCount++;
-            textBox.textContent = 'Second Player\'s turn O'
+            textBox.textContent = `${secondPlayer.name}` + `\'s turn!`;
         }else {
             cell.style.color = 'black';
             gameboard.board[cell.id-1] = 'O';
             renderGameboard();
             cell.style.pointerEvents = 'none'; 
             turnCount++;
-            textBox.textContent = 'First Player\'s turn X'
+            textBox.textContent = `${firstPlayer.name}` + `\'s turn!`;
         }
 
         let result = checkForWin();
@@ -112,18 +113,31 @@ cells.forEach((cell) => {
 restartButton.addEventListener('click', restartGame);
 
 function restartGame() {
-    cells.forEach((cell) => {
-        cell.textContent = '';
-        cell.style.pointerEvents = 'auto';
-    })
-    turnCount = 1;
-    gameboard.board = ['', '', '', '', '', '', '', '', '' ];
-    textBox.textContent = 'First Player\'s turn X';
-    restartButton.style.display = 'none';
-    let canvasRestart = 'clear';
-    drawLine(canvasRestart);
-    canvas.style.display = 'none';
-    
+    if( gameMode == 'multiPlayerMode') {
+        cells.forEach((cell) => {
+            cell.textContent = '';
+            cell.style.pointerEvents = 'auto';
+        })
+        turnCount = 1;
+        gameboard.board = ['', '', '', '', '', '', '', '', '' ];
+        textBox.textContent = 'Let\'s play Tic-Tac-Toe!';
+        restartButton.style.display = 'none';
+        let canvasRestart = 'clear';
+        drawLine(canvasRestart);
+        canvas.style.display = 'none';   
+    }else if (gameMode == 'singlePlayerMode'){
+        cellsAI.forEach((cell) => {
+            cell.textContent = '';
+            cell.style.pointerEvents = 'auto';
+        })
+        turnCount = 1;
+        gameboard.board = ['', '', '', '', '', '', '', '', '' ];
+        textBoxAI.textContent = 'Let\'s play Tic-Tac-Toe!';
+        restartButton.style.display = 'none';
+        let canvasRestart = 'clear';
+        drawLine(canvasRestart);
+        canvasAI.style.display = 'none';   
+    } 
 };
 
 // -------------------------- CHECK FOR WINNER -----------------------------------
@@ -174,58 +188,112 @@ function checkForWin() {
 // -------------------------- DRAW LINE OVER WINNING COMBO -------------------------
 
 function drawLine(combo) {
-    let ctx = canvas.getContext('2d');
-    ctx.lineWidth = 8;
-    ctx.beginPath();
-    ctx.strokeStyle = 'limegreen';
 
-    switch (combo) {
-        case '0':
-            ctx.moveTo(0,50);
-            ctx.lineTo(300,50);
-            ctx.stroke();
-            break;
-        case '1':
-            ctx.moveTo(0,150);
-            ctx.lineTo(300,150);
-            ctx.stroke();
-            break;
-        case '2':
-            ctx.moveTo(0,250);
-            ctx.lineTo(300,250);
-            ctx.stroke();
-            break;
-        case '3':
-            ctx.moveTo(50,0);
-            ctx.lineTo(50,300);
-            ctx.stroke();
-            break;
-        case '4':
-            ctx.moveTo(150,0);
-            ctx.lineTo(150,300);
-            ctx.stroke();
-            break;
-        case '5':
-            ctx.moveTo(250,0);
-            ctx.lineTo(250,300);
-            ctx.stroke();
-            break;
-        case '6':
-            ctx.moveTo(0,0);
-            ctx.lineTo(300,300);
-            ctx.stroke();
-            break;
-        case '7':
-            ctx.moveTo(300,0);
-            ctx.lineTo(0,300);
-            ctx.stroke();
-            break; 
-        case 'clear':
-            ctx.clearRect(0,0,300,300);
-            break;
+    if (gameMode == 'multiPlayerMode') {
+        let ctx = canvas.getContext('2d');
+        ctx.lineWidth = 8;
+        ctx.beginPath();
+        ctx.strokeStyle = 'limegreen';
+
+        switch (combo) {
+            case '0':
+                ctx.moveTo(0,50);
+                ctx.lineTo(300,50);
+                ctx.stroke();
+                break;
+            case '1':
+                ctx.moveTo(0,150);
+                ctx.lineTo(300,150);
+                ctx.stroke();
+                break;
+            case '2':
+                ctx.moveTo(0,250);
+                ctx.lineTo(300,250);
+                ctx.stroke();
+                break;
+            case '3':
+                ctx.moveTo(50,0);
+                ctx.lineTo(50,300);
+                ctx.stroke();
+                break;
+            case '4':
+                ctx.moveTo(150,0);
+                ctx.lineTo(150,300);
+                ctx.stroke();
+                break;
+            case '5':
+                ctx.moveTo(250,0);
+                ctx.lineTo(250,300);
+                ctx.stroke();
+                break;
+            case '6':
+                ctx.moveTo(0,0);
+                ctx.lineTo(300,300);
+                ctx.stroke();
+                break;
+            case '7':
+                ctx.moveTo(300,0);
+                ctx.lineTo(0,300);
+                ctx.stroke();
+                break; 
+            case 'clear':
+                ctx.clearRect(0,0,300,300);
+                break;
+        }
+    }else if (gameMode == 'singlePlayerMode') {
+        let ctx = canvasAI.getContext('2d');
+        ctx.lineWidth = 8;
+        ctx.beginPath();
+        ctx.strokeStyle = 'limegreen';
+
+        switch (combo) {
+            case '0':
+                ctx.moveTo(0,50);
+                ctx.lineTo(300,50);
+                ctx.stroke();
+                break;
+            case '1':
+                ctx.moveTo(0,150);
+                ctx.lineTo(300,150);
+                ctx.stroke();
+                break;
+            case '2':
+                ctx.moveTo(0,250);
+                ctx.lineTo(300,250);
+                ctx.stroke();
+                break;
+            case '3':
+                ctx.moveTo(50,0);
+                ctx.lineTo(50,300);
+                ctx.stroke();
+                break;
+            case '4':
+                ctx.moveTo(150,0);
+                ctx.lineTo(150,300);
+                ctx.stroke();
+                break;
+            case '5':
+                ctx.moveTo(250,0);
+                ctx.lineTo(250,300);
+                ctx.stroke();
+                break;
+            case '6':
+                ctx.moveTo(0,0);
+                ctx.lineTo(300,300);
+                ctx.stroke();
+                break;
+            case '7':
+                ctx.moveTo(300,0);
+                ctx.lineTo(0,300);
+                ctx.stroke();
+                break; 
+            case 'clear':
+                ctx.clearRect(0,0,300,300);
+                break;
+        }
     }
-
 }
+    
 
 function extractMark(result){
     return result.slice(0,1);
@@ -236,10 +304,138 @@ function extractCombo(result){
 }
 
 
+// --------------------------------------- MAIN MENU ----------------------------------------------------
+
+const mainMenuWindow = document.querySelector('.main-menu');
+const mainMenuButton = document.getElementById('main-menu-button');
+const singlePlayerWindow = document.querySelector('.singleplayer');
+const multiPlayerWindow = document.querySelector('.multiplayer')
+const confirmButton = document.getElementById('confirm-button');
+const singlePlayerButton = document.getElementById('singleplayer-button');
+const multiplayerButton = document.getElementById('multiplayer-button');
+const playerNameInputWindow = document.getElementById('playerNamesInput');
+const difficultyOptionsWindow = document.getElementById('difficultyOptions');
+let gameMode = '';
+
+
+mainMenuButton.addEventListener('click', ()=> {
+    restartGame();
+    multiplayerButton.style.border = '1px solid black';
+    singlePlayerButton.style.border = '1px solid black';
+    singlePlayerWindow.style.display = 'none';
+    difficultyOptionsWindow.style.display = 'none';
+    multiPlayerWindow.style.display = 'none';
+    playerNameInputWindow.style.display = 'none';
+    mainMenuWindow.style.display = 'flex';
+});
+
+singlePlayerButton.addEventListener('click', ()=> {
+    difficultyOptionsWindow.style.display = 'block';
+    singlePlayerButton.style.border = '2px solid limegreen';
+    multiplayerButton.style.border = '1px solid black';
+    playerNameInputWindow.style.display = 'none';
+    gameMode = 'singlePlayerMode';
+});
+
+multiplayerButton.addEventListener('click', () => {
+    playerNameInputWindow.style.display = 'block';
+    singlePlayerButton.style.border = '1px solid black';
+    multiplayerButton.style.border = '2px solid limegreen';
+    difficultyOptionsWindow.style.display = 'none';
+    gameMode = 'multiPlayerMode';
+});
+
+confirmButton.addEventListener('click', () => {    
+    if (gameMode === 'singlePlayerMode'){
+        mainMenuWindow.style.display = 'none';
+        singlePlayerWindow.style.display = 'flex';
+    }else if(gameMode === 'multiPlayerMode'){
+        mainMenuWindow.style.display = 'none';
+        multiPlayerWindow.style.display = 'flex';
+    }
+});
+
+
+// -------------------------------- SINGLE PLAYER MODE --------------------------------
+
+const cellsAI = document.querySelectorAll('.cell-ai');
+const textBoxAI = document.getElementById('text-box-ai');
+const canvasAI = document.getElementById('myCanvas-ai');
+
+cellsAI.forEach((cell) => {
+
+    cell.addEventListener('mouseenter', function cellEnter() {
+        if(cell.textContent === ''){
+            if ((turnCount % 2) == 1) {
+                cell.style.color = 'lightgrey';
+                cell.textContent = 'X';
+            }else {
+                cell.style.color = 'lightgrey';
+                cell.textContent = 'O';
+            }
+        }
+    })
+
+    cell.addEventListener('mouseleave', function cellLeave() {
+        if(cell.style.color === 'lightgrey') {
+            cell.textContent = '';
+            cell.style.color = 'black';
+        }
+    })
+
+    cell.addEventListener('click', function handleClick(){
+
+        if ((turnCount % 2) == 1) {
+            cell.style.color = 'black';
+            let cellIndex = cell.id.slice(0,1);
+            gameboard.board[cellIndex-1] = 'X';
+            renderGameboard();
+            cell.style.pointerEvents = 'none';          
+            turnCount++;
+        }
+        if ((turnCount % 2) == 0 && (turnCount < 10)) {
+            cellIndex = computerPlay();
+            let cellAI = document.getElementById(`${cellIndex + 1}-ai`);
+            cellAI.style.color = 'black';
+            gameboard.board[cellIndex] = 'O';
+            renderGameboard();
+            turnCount++;
+        }
+
+        let result = checkForWin();
+        if (result !== undefined) {
+            let winnerMark = extractMark(result);
+            let combo = extractCombo(result);
+            if (winnerMark === 'X'){
+                textBoxAI.textContent = `You win!`;
+                canvasAI.style.display = 'block';
+                drawLine(combo);
+                restartButton.style.display = 'block';
+            }else if (winnerMark === 'O') {
+                textBoxAI.textContent = `You lose!`;
+                canvasAI.style.display = 'block';
+                drawLine(combo);
+                restartButton.style.display = 'block';
+            } 
+        }else if((result === undefined) && (turnCount == 10)) {
+            textBoxAI.textContent = `Draw!`;
+            restartButton.style.display = 'block';
+        }
+    });
+});
+
+function computerPlay() {
+    let computerChoicesArray = [];
+    let currentBoard = gameboard.board;
+    for ( let i = 0; i < gameboard.board.length ; i ++) {
+        if(gameboard.board[i] == ''){
+            computerChoicesArray.push(i);
+        }
+    }
+    let randomChoice = Math.floor(Math.random() * computerChoicesArray.length);
+    let compChoice = computerChoicesArray[randomChoice];    
+    return compChoice;
+}
 
 // Notes:
-// - Fix draw issue at end game (done)
-// - Remove clickability after someone wins (done)
-// - Have restart button appear after someone wins (done)
-// - Have a line appear over winning combo (done)
-// - Fix hovering effect (done)
+// - Remove clickability on O square in single-player
